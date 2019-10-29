6af42b1554149f4d8d5e621dbf1be80d73dd7d52
UKRoo <ivashko.dmytro@gmail.com>
2019-10-29 16:57:49 +0200

    1 lessons

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// alert("hello world");
+// let a = prompt("Enter a value");
+// console.log("A =", a);
+// Number
+// String
+// Boolean
+// null
+// undefined
+// // Symbol
+
+//  Обєкт не порівнавати по typeof
+
+// let c = a + b;
+// console.log(c);
